efcae56d846e3eebbfe36fa67663cc5a1d1b7f7e
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-22 02:19:58 +0530

    Create thingy.bat

---
+++ thingy.bat
@@ -0,0 +1,8 @@
+@echo off
+title the @ttic
+CLS
+for /f "tokens=*" %%i in ('echo on ^& prompt $H ^&  echo^|cmd') do set the_thingy=%%i
+echo|set /p=WAKE UP NEO&(for /l %%i in (1,1,10) do powershell -c "write-host -nonewline \"%the_thingy%`b\"")
+timeout 1 >NUL
+cls
+echo :)
